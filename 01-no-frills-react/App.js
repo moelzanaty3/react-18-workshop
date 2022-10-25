19a1928d7cefbeed97ecf699a12463f907a7e3ee
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-10-25 19:21:12 +0200

    feat(no-frills-react): components [props]

--- 01-no-frills-react/App.js
+++ 01-no-frills-react/App.js
@@ -1,18 +1,26 @@
 // Pet Component
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement('div', {}, [
-    React.createElement('h1', {}, 'Luna'),
-    React.createElement('h2', {}, 'Dog'),
-    React.createElement('h2', {}, 'Havanese'),
+    React.createElement('h1', {}, props.name),
+    React.createElement('h2', {}, props.animal),
+    React.createElement('h2', {}, props.breed),
   ]);
 };
 // App Component
 const App = () => {
   return React.createElement('div', {}, [
     React.createElement('h1', {}, 'Adopt Me!'),
-    React.createElement(Pet),
-    React.createElement(Pet),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: 'Luna',
+      animal: 'Dog',
+      breed: 'Havanese',
+    }),
+    React.createElement(Pet, {
+      name: 'Pepper',
+      animal: 'Bird',
+      breed: 'Cockatiel',
+    }),
+    React.createElement(Pet, { name: 'Doink', animal: 'Cat', breed: 'Mix' }),
   ]);
 };
 // Get Root Element
