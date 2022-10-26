0f67647d31f6ba0e35a67232a71e99b1bdb8da03
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-10-26 15:48:07 +0200

    feat(react-capabilities): react router dom

--- 06-react-router-dom/src/App.jsx
+++ 06-react-router-dom/src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import SearchParams from './components/SearchParams';
+import Details from './components/Details';
+
+const NotFound = () => <h1>Page Not Found</h1>;
 
 // App Component
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <header>
+        <Link to="/">Adopt Me!</Link>
+      </header>
+      <Routes>
+        <Route path="/details/:id" element={<Details />} />
+        <Route path="/" element={<SearchParams />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 // Get Root Element
