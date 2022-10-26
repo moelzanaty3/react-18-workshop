fd73f927116fe19a63649aed1acc9894c9f2a1cc
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2022-10-26 11:03:56 +0200

    feat(core-react): hooks useState

--- 04-hooks/src/App.jsx
+++ 04-hooks/src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Pet from './components/Pet';
+import SearchParams from './components/SearchParams';
 
 // App Component
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Havanese" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
---
+++ 04-hooks/src/components/SearchParams.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+
+const ANIMALS = ['bird', 'cat', 'dog', 'rabbit', 'reptile'];
+
+const SearchParams = () => {
+  const [location, setLocation] = useState('');
+  const [animal, setAnimal] = useState('');
+  const [breed, setBreed] = useState('');
+
+  const breeds = [];
+
+  const handleLocationChange = (e) => {
+    setLocation(e.target.value);
+  };
+
+  const handleAnimalChange = (e) => {
+    setAnimal(e.target.value);
+    setBreed('');
+  };
+
+  const handleBreedChange = (e) => {
+    setBreed(e.target.value);
+  };
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={handleLocationChange}
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select id="animal" value={animal} onChange={handleAnimalChange}>
+            <option />
+            {ANIMALS.map((animal) => (
+              <option value={animal} key={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={!breeds.length}
+            id="breed"
+            value={breed}
+            onChange={handleBreedChange}
+          >
+            <option />
+            {breeds.map((breed) => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
